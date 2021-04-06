3af074a53be8ed3f386cfbb829460ee2297cc87a
Denis Holdeew <hold.denis@gmail.com>
2021-04-06 07:36:04 +0100

    Drop

--- src/components/Lane.vue
+++ src/components/Lane.vue
@@ -7,6 +7,9 @@
   >
     <div>
       <p>{{ lane.id }}</p>
+      <div v-for="person in lane.people" :key="person">
+        {{ person }}
+      </div>
     </div>
   </div>
   <button @click="$emit('removeLane', lane.id)">Remove lane</button>
@@ -15,12 +18,25 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+
 export default defineComponent({
   emits: ["removeLane"],
   props: {
     lane: {
       type: Object,
       required: true,
+    },
+  },
+  methods: {
+    startDrag(evt: any, item: any) {
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("itemID", "New Person");
+    },
+    onDrop(evt: any, person: any) {
+      // const itemID = evt.dataTransfer.getData("itemID");
+      // this.lane.people.push("New Person");
+      alert(`Dropped ${this.lane.people}`);
     },
   },
 });
