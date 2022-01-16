46072711edb31a6db70665015bc1fdb56fc68288
Denis Holdeew <hold.denis@gmail.com>
2022-01-16 16:48:10 +0100

    Add shim to access store using Composition API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import { store } from "./store";
+import { key, store } from "./store";
 import App from "./App.vue";
 import i18n from "./i18n";
 
-createApp(App).use(i18n).use(store).mount("#app");
+createApp(App).use(i18n).use(store, key).mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,14 @@
 import { User } from "@/models/User";
-import { createStore } from "vuex";
+import { createStore, Store } from "vuex";
 import { getAllUsers } from "@/services/UserService";
+import { InjectionKey } from "vue";
 
 export type State = {
   users: Array<User>;
 };
+
+// Enables the useStore function that's needed to access the store when using Composition API
+export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore({
   state(): State {
