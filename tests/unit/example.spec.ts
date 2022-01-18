97280f66ae2d6b186a4662f9abe52b26c03bd418
Denis Holdeew <hold.denis@gmail.com>
2022-01-18 19:56:46 +0100

    Add first unit test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,29 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import SidebarLeft from "@/components/SidebarLeft.vue";
+import Person from "@/components/Person.vue";
+import { User } from "@/models/User";
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe("SidebarLeft.vue", () => {
+  it("renders props.users when passed", () => {
+    const name1 = "Luka";
+    const name2 = "Bento";
+    const users: Array<User> = [
+      {
+        id: "1",
+        name: name1,
+      },
+      {
+        id: "1",
+        name: name2,
+      },
+    ];
+    const wrapper = shallowMount(SidebarLeft, {
+      props: { users },
     });
-    expect(wrapper.text()).to.include(msg);
+    // const text = wrapper.text();
+    // expect(text).to.contain(name1);
+    // expect(text).to.contain(name2);
+    expect(wrapper.findAllComponents(Person)).lengthOf(2);
   });
 });
