1e5403272b665bc79a7ae5ca74656ad6ac33c9db
Denis Holdeew <hold.denis@gmail.com>
2022-01-31 21:50:53 +0100

    Cleanup

--- src/components/PairingBoard.vue
+++ src/components/PairingBoard.vue
@@ -22,32 +22,24 @@
 import { defineComponent } from "@vue/runtime-core";
 import TaskArea from "./TaskArea.vue";
 import SidebarLeft from "./SidebarLeft.vue";
-import { User } from "@/models/User";
-import { ref, onMounted } from "vue";
+import { onMounted } from "vue";
 import { useStore } from "@/store";
-import { Task } from "@/models/Task";
 
 export default defineComponent({
   components: { TaskArea, SidebarLeft },
   setup() {
     const store = useStore();
-    const users = ref(new Array<User>());
-    const tasks = ref(new Array<Task>());
     const getAllUsers = async () => {
       await store.dispatch("getAllUsers");
-      users.value = store.state.users;
     };
     const getAllTasks = async () => {
       await store.dispatch("getAllTasks");
-      tasks.value = store.state.tasks;
     };
     onMounted(() => {
       getAllUsers();
       getAllTasks();
     });
     return {
-      users,
-      tasks,
       getAllUsers,
       getAllTasks,
     };
